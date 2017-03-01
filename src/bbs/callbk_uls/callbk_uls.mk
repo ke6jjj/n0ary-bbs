ff6fb70cdaae4ba42ec4d37a0924462439ec0ba7
Jeremy Cooper <jeremy@X.local>
2017-02-28 23:42:35 -0800

    Added forgotten component makefile fragment.

---
+++ src/bbs/callbk_uls/callbk_uls.mk
@@ -0,0 +1,17 @@
+# Get the directory in which this Makefile fragment resides.
+CALLBK_ULS_SRCDIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
+CALLBK_ULS_OBJDIR := $(BBS_OBJDIR)/callbk_uls
+
+CALLBK_MAKECB_ULS_APP_SRCDIR := $(CALLBK_ULS_SRCDIR)
+CALLBK_MAKECB_ULS_APP_OBJDIR := $(CALLBK_ULS_OBJDIR)
+
+CALLBK_MAKECB_ULS_APP_PROD := makecb_uls
+
+CALLBK_MAKECB_ULS_APP_SRCS := \
+	am_database.c call_id.c makecb_uls.c slab.c
+
+$(eval $(call create_bbs_app_rules,CALLBK_MAKECB_ULS_APP))
+
+BBS_PRODUCTS += $(CALLBK_MAKECB_ULS_APP)
+BBS_CLEAN_TARGETS += CALLBK_MAKECB_ULS_APP_CLEAN
+BBS_INSTALL_TARGETS += CALLBK_MAKECB_ULS_APP_INSTALL
