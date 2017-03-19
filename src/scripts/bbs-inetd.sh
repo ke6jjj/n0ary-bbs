d2d89a6967d15781ec6e7c6d8bedda5b1b0bdffb
Jeremy Cooper <jeremy.gthb@baymoo.org>
2017-03-19 14:35:57 -0700

    Updated inetd script from sources that I was actually using. Adds username filtering and other important changes that make the daemon actually work.

--- src/scripts/bbs-inetd.sh
+++ src/scripts/bbs-inetd.sh
@@ -2,15 +2,49 @@
 BBS_DIR=XXBBS_DIRXX
 BBS_BIN=${BBS_DIR}/bin/b_bbs
 
+#
+# Issue a login prompt and read the username.
+#
 echo ""
 echo -n "login: "
-read username
+if ! read username; then
+  exit 0
+fi
+
+#
+# This script is run directly with the remote socket on stdin. This
+# means that the read command will pick up the traditional carriage
+# return character that is sent with every newline. We need to remove
+# it from the username received.
+#
+username=$( echo "$username" | sed -e 's,[^A-Za-z0-9]*,,g' )
+
 if [ "$username" == bbs ]; then
+  #
+  # User wishes to log in to BBS. Get a callsign.
+  #
   while :; do
     echo -n "Enter your callsign (first name if non-ham): "
-    read callsign
-    filt_callsign=$( echo "$callsign" | sed -e 's,[^A-Za-z0-9]*,,g' )
-    if [ "$filt_callsign" != "$callsign" ]; then
+    if ! read callsign; then
+      exit 1
+    fi
+
+    #
+    # As with the telnet username, the callsign must be filtered for
+    # trailing carriage returns.
+    #
+    nospc_callsign=$( echo "$callsign" | sed -e 's,[[:space:]]*,,g' )
+
+    #
+    # Additionally, we need to keep the callsign to alphanumeric characters
+    # only.
+    #
+    filt_callsign=$( echo "$nospc_callsign" | sed -e 's,[^A-Za-z0-9]*,,g' )
+
+    #
+    # Warn the user if the callsign had bad characters in it.
+    #
+    if [ "$filt_callsign" != "$nospc_callsign" ]; then
       echo ""
       echo "Error in callsign. Only letters and numbers are allowed"
       echo "please try again."
@@ -19,7 +53,11 @@ if [ "$username" == bbs ]; then
     if [ "${filt_callsign}" == "" ]; then
       continue
     fi
-    echo "exec ${BBS_BIN} -t 1 -v TCP ${filt_callsign} 1<&0 2<&0"
+
+    #
+    # Run the BBS!
+    #
+    exec ${BBS_BIN} -t 1 -v TCP "${filt_callsign}" 1<&0 2<&0
   done
 fi
 exit 1
