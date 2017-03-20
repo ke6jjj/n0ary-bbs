25b2e082541107e9f41994496ec51d6710486636
Jeremy Cooper <jeremy.gthb@baymoo.org>
2017-03-20 02:09:06 -0700

    Fixed typo that caused BBS_CALL not to be registered but BBS_HOST instead.

--- src/bbs/bbsd/main.c
+++ src/bbs/bbsd/main.c
@@ -229,7 +229,7 @@ fetch_configuration(void)
 		Bbs_Host = s;
 
 	if((s = config_fetch("BBS_CALL")) != NULL)
-		Bbs_Host = s;
+		Bbs_Call = s;
 
 	if((s = config_fetch("BBSD_PORT")) != NULL) {
 		if(!isdigit(*s)) {
