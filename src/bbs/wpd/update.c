5ebea686c2e568104950b14d1dc5686fc426cef5
Jeremy Cooper <jeremy.gthb@baymoo.org>
2017-03-27 17:49:43 -0700

    Reorganized WP update process so that both the global and the local distribution types can be optional.

--- src/bbs/wpd/update.c
+++ src/bbs/wpd/update.c
@@ -19,58 +19,47 @@ generate_wp_update(struct active_processes *ap)
 	FILE *lfp = NULL, *gfp;
 	int gcnt, lcnt, gfd, lfd;
 
-	strcpy(gfn, "/tmp/wpupdate.g.XXXXXX");
+	if (Wpd_Global_Server == NULL && Wpd_Local_Distrib == NULL)
+		/* Don't send updates anywhere */
+		return OK;
 
+	/* Create temporary file for global update */
+	strcpy(gfn, "/tmp/wpupdate.g.XXXXXX");
 	if ((gfd = mkstemp(gfn)) < 0)
 		return ERROR;
-
 	if((gfp = fopen(gfn, "w")) == NULL) {
 		close(gfd);
 		return ERROR;
 	}
-
 	close(gfd);
 
-	if(Wpd_Local_Distrib) {
-		strcpy(lfn, "/tmp/wpupdate.l.XXXXXX");
-		if ((lfd = mkstemp(lfn)) < 0) {
-			fclose(gfp);
-			unlink(gfn);
-			return ERROR;
-		}
-		if ((lfp = fopen(lfn, "w")) == NULL) {
-			fclose(gfp);
-			unlink(gfn);
-			close(lfd);
-			unlink(lfn);
-			return ERROR;
-		}
+	/* Create temporary file for local update */
+	strcpy(lfn, "/tmp/wpupdate.l.XXXXXX");
+	if ((lfd = mkstemp(lfn)) < 0) {
+		fclose(gfp);
+		unlink(gfn);
+		return ERROR;
+	}
+	if ((lfp = fopen(lfn, "w")) == NULL) {
+		fclose(gfp);
+		unlink(gfn);
 		close(lfd);
-		l_smtpmsg = malloc_struct(smtp_message);
+		unlink(lfn);
+		return ERROR;
 	}
-
-	g_smtpmsg = malloc_struct(smtp_message);
-
-	strcpy(buf, Wpd_Global_Server);
-	uppercase(buf);
-	smtp_add_recipient(g_smtpmsg, buf, SMTP_REAL);
-
-	if(l_smtpmsg != NULL) {
-		strcpy(buf, Wpd_Local_Distrib);
-		uppercase(buf);
-		smtp_add_recipient(l_smtpmsg, buf, SMTP_REAL);
-		smtp_add_sender(l_smtpmsg, Bbs_Call);
-	}
-
-	smtp_add_sender(g_smtpmsg, Bbs_Call);
+	close(lfd);
 
 	hash_gen_update(gfp, &gcnt, lfp, &lcnt);
 
 	fclose(gfp);
-	if(lfp)
-		fclose(lfp);
+	fclose(lfp);
 
-	if(gcnt) {
+	if (Wpd_Global_Server != NULL && gcnt > 0) {
+		g_smtpmsg = malloc_struct(smtp_message);
+		strlcpy(buf, Wpd_Global_Server, sizeof(buf));
+		uppercase(buf);
+		smtp_add_recipient(g_smtpmsg, buf, SMTP_REAL);
+		smtp_add_sender(g_smtpmsg, Bbs_Call);
 		gfp = fopen(gfn, "r");
 		smtp_body_from_file(g_smtpmsg, gfp);
 		fclose(gfp);
@@ -82,30 +71,34 @@ generate_wp_update(struct active_processes *ap)
 			msg_generate(g_smtpmsg, sendBULLETIN);
 			exit(0);
 		}
+		smtp_free_message(g_smtpmsg);
+		free(g_smtpmsg);
 	}
 
-	smtp_free_message(g_smtpmsg);
-	free(g_smtpmsg);
 	unlink(gfn);
 
-	if(l_smtpmsg != NULL) {
-		if(lcnt) {
-			lfp = fopen(lfn, "r");
-			smtp_body_from_file(l_smtpmsg, lfp);
-			fclose(lfp);
-			smtp_set_subject(l_smtpmsg, Wpd_Update_Subject);
-			smtp_set_max_size(l_smtpmsg, Wpd_Update_Size);
-			if(ap != NULL)
-				disp_update(ap, l_smtpmsg);
-			if(fork() == 0) {
-				msg_generate(l_smtpmsg, sendPRIVATE);
-				exit(0);
-			}
+	if (Wpd_Local_Distrib != NULL && lcnt > 0) {
+		l_smtpmsg = malloc_struct(smtp_message);
+		strcpy(buf, Wpd_Local_Distrib);
+		uppercase(buf);
+		smtp_add_recipient(l_smtpmsg, buf, SMTP_REAL);
+		smtp_add_sender(l_smtpmsg, Bbs_Call);
+		lfp = fopen(lfn, "r");
+		smtp_body_from_file(l_smtpmsg, lfp);
+		fclose(lfp);
+		smtp_set_subject(l_smtpmsg, Wpd_Update_Subject);
+		smtp_set_max_size(l_smtpmsg, Wpd_Update_Size);
+		if(ap != NULL)
+			disp_update(ap, l_smtpmsg);
+		if(fork() == 0) {
+			msg_generate(l_smtpmsg, sendPRIVATE);
+			exit(0);
 		}
-
 		smtp_free_message(l_smtpmsg);
 		free(l_smtpmsg);
-		unlink(lfn);
 	}
+
+	unlink(lfn);
+
 	return OK;
 }
