ba28bd67348f9b7e28d622d9ceadb5f5e5f3ac72
Jeremy Cooper <jeremy.gthb@baymoo.org>
2025-05-07 16:27:56 -0700

    Add KISS PCAP dumping ability to TNCD.
    Add the ability to keep a long-running log of all packets received and
    transmitted on a TNC through tcpdump/WireShark-compatible "PCAP" files.

--- src/bbs/tncd/kiss.h
+++ src/bbs/tncd/kiss.h
@@ -1,11 +1,14 @@
 #ifndef _TNCD_KISS_H
 #define _TNCD_KISS_H
 
+#include <stdio.h>
+
 #include "calls.h"
 
 typedef struct kiss kiss;
 
 kiss *kiss_init(void);
+int kiss_set_pcap_dump(kiss *dev, FILE *fp);
 int kiss_raw(kiss *dev, struct mbuf *data);
 int kiss_recv(void *dev, struct mbuf *bp);
 int kiss_set_send(kiss *dev, mbuf_recv_fn send_fn, void *send_arg);
